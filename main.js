8c596401d6ff096696c53ef6da7df9f6d5a5415b
Hatter <vu.nht00@gmail.com>
2023-10-14 13:24:54 +0700

    finish website

--- main.js
+++ main.js
@@ -1,2 +1,49 @@
-import './style.css'
+import "./style.css";
 
+const form = document.querySelector("form");
+const signupForm = document.querySelector("[signup-form]");
+const successDiv = document.querySelector("[success]");
+const emailInput = document.querySelector("input[type=email]");
+const emailErrorText = document.querySelector("[error-text]");
+const dismissBtn = document.querySelector("[dismiss-btn]");
+
+signupForm.style.display = 'none'
+successDiv.style.display = 'flex';
+
+
+function validateEmail(inputText) {
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value === '') {
+        return false;
+    }
+
+    if (inputText.value.match(mailFormat)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    const email = document.querySelector("input#email");
+
+    const valid = validateEmail(email);
+
+    if(valid) {
+        signupForm.style.display = 'none';
+        successDiv.style.display = 'flex';
+        emailInput.classList.remove('invalid');
+        emailErrorText.classList.remove('block');
+        emailErrorText.classList.add('hidden');
+    } else {
+        emailInput.classList.add('invalid');
+        emailErrorText.classList.add('block');
+        emailErrorText.classList.remove('hidden');
+    }
+};
+
+dismissBtn.onclick = () => {
+    signupForm.style.display = 'inline-flex';
+    successDiv.style.display = 'none';
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+import { colors as defaultColors } from "tailwindcss/defaultTheme";
+
+const colors = {
+  ...defaultColors,
+  ...{
+    "tomato": "hsl(4, 100%, 67%)",
+    "dark-slate-grey": "hsl(234, 29%, 20%)",
+    "charcoal-grey": "hsl(235, 18%, 26%)",
+    "light-blue": "hsl(206, 94%, 87%)",
+    "grey": "hsl(231, 7%, 60%)",
+    white: "hsl(0, 0%, 100%)",
+  },
+};
+
 export default {
   content: [
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors
+    },
   },
   plugins: [],
 }
